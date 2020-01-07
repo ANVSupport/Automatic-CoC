5c2910f23cdf6873cd66f3d8763bf0a9757855e1
ANVSupport <giladb@anyvision.co>
2020-01-07 16:52:12 +0200

    Adds local logging

--- checks.py
+++ checks.py
@@ -6,15 +6,26 @@ from decimal import Decimal
 import re
 import json
 
-def file_modifications_checks():
+""" 			Logger Block, sets up general Logging.			"""
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)-15s [%(levelname)s] %(funcName)s: %(message)s")
+file_handler = logging.FileHandler('coc.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+
+
+
+def Check_Modified_Files():
 	""" 
 	Sets File Paths
 	Needs to become more generic, right now only for Safeguard (1.20.0)
 
 	"""
+	yaml_json = {}
 	hostname = subprocess.check_output("hostname").decode("utf-8").replace("\n","")
 	logname = subprocess.check_output("logname").decode("utf-8").replace("\n","")
-
 
 	docker_compose_yaml  = "/home/%s/docker-compose/1.20.0/docker-compose.yml" % logname
 	profile_file = "/home/%s/.profile" % logname
@@ -29,27 +40,28 @@ def file_modifications_checks():
 	broadcaster_searching_lines = ["BCAST_MODBUS_IS_ENABLED=true", 
 	"BCAST_MODBUS_CMD_PATH=/home/user/moxa-config/moxa_e1214.sh", 
 	"BCAST_MODBUS_CAMERA_LIST_PATH=/home/user/moxa-config/cameraList.json"]
+	try:
+		with open(docker_compose_yaml, 'r') as stream:
+			content = stream.read()
+			nginx_count = content.count(nginx_hostname)
+			api_count = content.count(api_hostname)
+			moxa_mount_count = content.count(moxa_mount_path)
 
-	with open(docker_compose_yaml, 'r') as stream:
-		content = stream.read()
-		nginx_count = content.count(nginx_hostname)
-		api_count = content.count(api_hostname)
-		moxa_mount_count = content.count(moxa_mount_path)
+		with open(profile_file, 'r') as stream:
+			content = stream.read()
+			xhost_count = content.find("xhost +")
+			if xhost_count is -1:
+				xhost_count = "Not found"
 
-	with open(profile_file, 'r') as stream:
-		content = stream.read()
-		xhost_count = content.find("xhost +")
-		if xhost_count is -1:
-			xhost_count = "Not found"
-
-	with open(broadcaster_file, 'r') as stream:
-		content = stream.read()
-		are_broadcaster_lines_correct = True
-		for line in broadcaster_searching_lines:
-			if content.find(line) is -1:
-				are_broadcaster_lines_correct = False
-
-	yaml_json = {}
+		with open(broadcaster_file, 'r') as stream:
+			content = stream.read()
+			are_broadcaster_lines_correct = True
+			for line in broadcaster_searching_lines:
+				if content.find(line) is -1:
+					are_broadcaster_lines_correct = False
+	except FileNotFoundError as err:
+		logger.error("Error While Openig YAML and profile files: \n \t\t %s" % (err))
+		return yaml_json
 	yaml_json["hostname"] = hostname
 	yaml_json[nginx_hostname] = nginx_count
 	yaml_json[api_hostname] = api_count
@@ -58,25 +70,22 @@ def file_modifications_checks():
 	yaml_json["Are Broadcaster edits present"] = are_broadcaster_lines_correct
 	return yaml_json
 
-
-
-def storage_mount_checks():
+def Check_Storage_Mount():
 	fstab_file = "/etc/fstab"
-	entire_line = "No Matching Entry Found"
+	entire_line = None
 	with open(fstab_file, 'r') as stream:
 		content = stream.read()
 		for line in content.split("\n"):
 			if "storage" in line:
 				entire_line = line.strip()
 	fstab_json = {}
+	if entire_line is None:
+		logger.warning("No Matching Fstab Entry Found")
+		entire_line  = "No Matching Entry Found"
 	fstab_json["/etc/fstab entry"] = entire_line
 	return fstab_json
 
-
-
-
-
-def hw_checks():
+def Get_Hardware_Specifications():
 
 	cpu_model = None
 	ram_capacity = None
@@ -84,25 +93,27 @@ def hw_checks():
 	gpu_model = None
 
 	# CPU Block
-	cpu_p1 = subprocess.Popen(["cat", "/proc/cpuinfo"], stdout=subprocess.PIPE) 							# pipe into cpu_p2 stdin
-	cpu_p2 = subprocess.Popen(["grep", "-m1", "model name"], stdin=cpu_p1.stdout, stdout=subprocess.PIPE) 	# find cpu name
-	cpu_p1.stdout.close() 																					# close pipe if error
-	cpu_model = clean_string(str(cpu_p2.communicate()[0]).split(":")[1])							     	#execute command
-
+	try:
+		cpu_p1 = subprocess.Popen(["cat", "/proc/cpuinfo"], stdout=subprocess.PIPE) 							# pipe into cpu_p2 stdin
+		cpu_p2 = subprocess.Popen(["grep", "-m1", "model name"], stdin=cpu_p1.stdout, stdout=subprocess.PIPE) 	# find cpu name
+		cpu_p1.stdout.close() 																					# close pipe if error
+		cpu_model = Clean(str(cpu_p2.communicate()[0]).split(":")[1])							     	#execute command
+	except:
+		logger.warning("Could not Get CPU Name")		
 	# GPU Block
 	try:
 		gpu_model = subprocess.Popen(["nvidia-smi", "--list-gpus"], stdout=subprocess.DEVNULL)
 		gpu_model = str(gpu_model.communicate()[0])
 	except:
+		logger.warning("Can't cummunicate with nvidia GPU")
 		try:
 			gpu_model_p1 = subprocess.Popen(["lspci"], stdout=subprocess.PIPE)
 			gpu_model_p2 = subprocess.Popen(["grep", "-i", "vga"], stdin=gpu_model_p1.stdout, stdout=subprocess.PIPE)
 			gpu_model_p1.stdout.close()
 			gpu_model = gpu_model_p2.communicate()[0]
 		except:
-			gpu_model = "Failed to Fetch GPU Info"
-			# TODO Logging, no driver installed
-	gpu_model = clean_string(gpu_model)
+			logger.error("Could not Fetch GPU Info")
+	gpu_model = Clean(gpu_model)
 
 
 	# RAM Block
@@ -113,9 +124,9 @@ def hw_checks():
 		ram_capacity_KB = Decimal(str(ram_capacity_p2.communicate()[0]).split()[1])
 		ram_capacity_GB = Decimal(ram_capacity_KB/1024)
 		ram_capacity_GB = round(ram_capacity_GB,2)
-		ram_capacity_GB = clean_string(ram_capacity_GB)
+		ram_capacity_GB = Clean(ram_capacity_GB)
 	except:
-		ram_capacity = "Failed to Fetch RAM capacity"
+		logger.error("Could Not Fetch RAM Capacity")
 
 	# HDD Block
 	try:
@@ -123,11 +134,9 @@ def hw_checks():
 		hdd_p2 = subprocess.Popen(["grep", "-i", "disk"], stdin=hdd_p1.stdout, stdout=subprocess.PIPE)
 		hdd_p1.stdout.close()
 		hard_drives = str(hdd_p2.communicate()[0])
-		hard_drives = clean_string(hard_drives)
+		hard_drives = Clean(hard_drives)
 	except:
-		print("Couldn't Fetch HDD Info")
-		# TODO Logging
-	#Builds JSON to return to main
+		logger.error("Could not Fetch HDD Info")
 
 	specs_json = {}
 	specs_json["CPU Model"] = cpu_model
@@ -137,11 +146,7 @@ def hw_checks():
 
 	return specs_json
 
-
-
-
-
-def check_installed_apps(app_list):
+def Check_Installed_Apps(app_list):
 	installed_apps=[]
 	not_installed_apps=[]
 	for app in app_list:
@@ -152,35 +157,35 @@ def check_installed_apps(app_list):
 			not_installed_apps.append(app)
 	for index, missing_app in enumerate(not_installed_apps):
 		try:
-			print("Installing %s... Please Wait" % (missing_app,))
+			print("Installing %s... Please Wait \n\n" % (missing_app,))
 			installation = subprocess.run(["sudo", "apt-get", "install", "-yqq", missing_app])
 			if installation.returncode is 0:
 				installed_apps.append(missing_app)
-				print("%s installed" % (missing_app,))
+				logger.info("Installed %s using apt-get" % (missing_app))
 				not_installed_apps[index] = None
 			else:
-				print("error installing %s, please install manually" % (app,))
+				logger.error("could not install %s, return code: %d" % (missing_app, installation.returncode))
 		except Exception as err:
-				# TODO Log failure
-				# TODO orginize tryblock vs if else
-				print("couldn't install")
-				print(err)
+				logger.error("Could not Install %s, Exception: %s" % (missing_app, err))
 
 
 	installed_apps_json = {}
+	number_of_installed_apps = 0
+	number_of_not_installed_apps = 0
 	for installed_app in installed_apps:
 		installed_apps_json[installed_app] = " .....Installed    V"
+		number_of_installed_apps = number_of_installed_apps+1
 
 	for not_installed_app in not_installed_apps:
 		installed_apps_json[not_installed_app] = ".....Not Installed    X"
+		number_of_not_installed_apps = number_of_not_installed_apps+1
+	logger.info("Installed apps: %d" % (number_of_installed_apps))
+	logger.info("Not Installed Apps: %d" % (number_of_not_installed_apps))
 		
 
 	return installed_apps_json
 
-
-
-
-def license_check():
+def Check_License():
 	# Needs testing on Various BT machines...
 	footprint = None
 	expiration = None
@@ -190,8 +195,7 @@ def license_check():
 		try:
 			backend_pod = subprocess.run("kubectl get pod", "grep edge-0", "awk '{print $1}'") # If it's kubernetes, extract backend pod
 		except:
-			pass
-			# TODO log failure
+			logger.error("Error While Extracting Backend Container/pod")
 		else:
 			footprint = str(subprocess.run("kubectl exec -it backend_pod -c edge -- bash -c '/usr/local/bin/license-ver -o' ").stdout)
 			expiration = str(subprocess.run("kubectl exec -it backend_pod -c edge -- bash -c '/usr/local/bin/license-ver -b' ").stdout)
@@ -200,11 +204,14 @@ def license_check():
 		expiration = str(subprocess.run(["docker", "exec", "-it", backend_container, "license-ver", "-b"]))
 	license_json = {}
 	license_json["System Footprint"] = footprint
-	license_json["License Expiration"] = parse_date(expiration)
+	license_json["License Expiration"] = Parse_Date(expiration)
 	return license_json
 
-def parse_date(messy_date):
-	date_list = list(messy_date)
+def Parse_Date(messy_date):
+	try:
+		date_list = list(messy_date)
+	except TypeError as err:
+		return "No Date"
 	Year = None
 	Months = None
 	Days = None
@@ -218,9 +225,7 @@ def parse_date(messy_date):
 		print(err)
 	return Date
 
-
-
-def clean_string(string):
+def Clean(string):
 	string =  str(string)
 	string = ' '.join(string.split()) # Replaces multipile white spaces with one.
 	string = string.replace("\\n", "")
--- main.py
+++ main.py
@@ -8,16 +8,16 @@ import json
 def main():
 	Report = {}
 	apps = ["git", "htop", "figlet"]
-	license_details = checks.license_check()
+	license_details = checks.Check_License()
 	if license_details is None:
 		license_details = "Error fetching license data.. is BT isntalled?"
 
 
-	Report["Hardware Details"] = checks.hw_checks()
-	Report["Installed Apps"] = checks.check_installed_apps(apps)
+	Report["Hardware Details"] = checks.Get_Hardware_Specifications()
+	Report["Installed Apps"] = checks.Check_Installed_Apps(apps)
 	Report["License"] = license_details
-	Report["Yaml Edits"] = checks.file_modifications_checks()
-	Report["Fstab Entry"] = checks.storage_mount_checks()
+	Report["Yaml Edits"] = checks.Check_Modified_Files()
+	Report["Fstab Entry"] = checks.Check_Storage_Mount()
 
 	formatted_Report = json.dumps(Report, indent=3)
 	print(formatted_Report)
